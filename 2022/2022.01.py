2b27d7e903b99c49300e3fd30cea7715224f13d1
dnell <mail@danienell.com>
2023-12-31 23:22:45 +0000

    update

--- 2022/2022.01.py
+++ 2022/2022.01.py
@@ -26,3 +26,15 @@ print(elf_dict)
 max_key = max(elf_dict, key=elf_dict.get)
 max_value = elf_dict[max_key]
 print(f"Max key: {max_key} and Max value: {max_value} ")
+
+
+# part two with sorting them.
+import pandas as pd
+
+df = pd.DataFrame(list(elf_dict.items()), columns=['Key', 'Value'])
+sorted_df = df.sort_values(by="Value", ascending=False)
+
+top3 = sorted_df.head(3)
+
+sum_top3 = top3["Value"].sum()
+print(sum_top3)
